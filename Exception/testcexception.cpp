2a4da0c3d8825e32b94910531f43745ce64f3123
ROUX Gwenaël <37573231+Gwenael-ROUX@users.noreply.github.com>
2018-04-06 14:53:26 +0200

    Add files via upload

---
+++ Exception/testcexception.cpp
@@ -0,0 +1,34 @@
+#include "exception.h"
+#include <string>
+#include <exception>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+using namespace nsUtil;
+
+namespace
+{
+    void TestException()
+    {
+        string Libelle;
+        unsigned CErr;
+        for(;;)
+        {
+            cout << "Entrer un libelle :" << endl;
+            cin >> Libelle;
+            if (cin.fail())
+                return;
+            cout << "Entrer un code erreur :" << endl;
+            cin >> CErr;
+            Exception Ex (Libelle, CErr);
+            Ex.display();
+        }
+    }
+}
+
+int main()
+{
+    TestException();
+    return 0;
+}
